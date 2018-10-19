f446dc2dce0cf9dd9238e83377b6abefbca86fcb
hemal patel <Hemal@hemals-MBP.attlocal.net>
2018-10-18 21:08:17 -0700

    changed startup

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -4,6 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace ManPurse
 {
@@ -26,6 +31,24 @@ namespace ManPurse
 
             // Add Cors
             services.AddCors();
+
+            services.AddAuthentication(JwtBearerDefaults.AddAuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new
+                TokenValidationParameters
+                {
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurity(System.Text.Encoding.UTF8.GetBytes("password"))
+
+                };
+
+            });
+        }
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +65,8 @@ namespace ManPurse
              app.UseCors(builder => builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader());
 
             // app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
-}
+
